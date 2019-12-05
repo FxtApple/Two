92c2a5aa1d267168ce1749a9855ffcb384a5e235
FxtApple <3448535297@qq.com>
2019-12-05 19:43:01 +0800

    FirstWeek

---
+++ src/FirstMonth_FirstWeek/OOPOOAOOD/PracticeOfPaint/Fengxt.java
@@ -0,0 +1,28 @@
+package FirstMonth_FirstWeek.OOPOOAOOD.PracticeOfPaint;
+
+class Fengxt {
+    /**
+     *年龄
+     * */
+    int age;
+
+    /**
+     * 性别
+     * */
+    String sex;
+
+    /***
+     * 姓名
+     */
+    String name;
+
+    /**
+     * 是否毕业
+     * */
+    boolean gratuate;
+
+    /**
+     * 余额
+     * */
+    long monny;
+}
